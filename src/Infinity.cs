95f787eca552afe2169b89671e0ed0e810a647ff
viclang <my.turn@aleeas.com>
2022-08-12 20:49:22 +0200

    Initial commit

---
+++ src/Infinity.cs
@@ -0,0 +1,206 @@
+﻿namespace InfinityComparable
+{
+    public partial struct Infinity<T> : IEquatable<Infinity<T>>, IComparable<Infinity<T>>, IComparable
+        where T : struct, IComparable<T>, IComparable
+    {
+        private readonly bool positive;
+        public T? Finite { get; } = null;
+        public bool IsInfinite => !Finite.HasValue;
+
+        public Infinity()
+        {
+            positive = true;
+        }
+
+        public Infinity(bool positive)
+        {
+            this.positive = positive;
+        }
+
+        public Infinity(T? value, bool positive)
+        {
+            Finite = value;
+            this.positive = positive;
+        }
+
+        public bool Equals(Infinity<T> other)
+        {
+            return this == other;
+        }
+
+        public override bool Equals(object? other)
+        {
+            if (other is Infinity<T>)
+            {
+                return this == (Infinity<T>)other;
+            }
+            if (!IsInfinite && other is not Infinity<T>)
+            {
+                return Finite.Equals(other);
+            }
+            if (IsInfinite && other is double)
+            {
+                return positive && ((double)other) == double.PositiveInfinity
+                    || !positive && ((double)other) == double.NegativeInfinity;
+            }
+            if (IsInfinite && other is float)
+            {
+                return positive && ((float)other) == float.PositiveInfinity
+                    || !positive && ((float)other) == float.NegativeInfinity;
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return IsInfinite ? positive ? 1 : -1
+                : Finite.GetHashCode();
+        }
+
+
+        public int CompareTo(Infinity<T> other)
+        {
+            if (IsInfinite && other.IsInfinite)
+            {
+                return positive.CompareTo(other.positive);
+            }
+            if (IsInfinite && !other.IsInfinite)
+            {
+                return positive ? 1 : -1;
+            }
+            if (!IsInfinite && other.IsInfinite)
+            {
+                return other.positive ? -1 : 1;
+            }
+            return Finite!.Value.CompareTo(other.Finite!.Value);
+        }
+
+        public int CompareTo(object? other)
+        {
+            if (IsInfinite && other is null)
+            {
+                return positive ? 1 : -1;
+            }
+            if (other is Infinity<T>)
+            {
+                return CompareTo((Infinity<T>)other);
+            }
+            if (IsInfinite && other is double && double.IsInfinity((double)other))
+            {
+                return (double)other == double.PositiveInfinity
+                    ? (positive ? 0 : -1)
+                    : (positive ? 1 : 0);
+            }
+            if (IsInfinite && other is float && float.IsInfinity((float)other))
+            {
+                return (float)other == double.PositiveInfinity
+                    ? (positive ? 0 : -1)
+                    : (positive ? 1 : 0);
+            }
+            if (IsInfinite && other is not null)
+            {
+                return positive ? 1 : -1;
+            }
+            return Finite!.Value.CompareTo(other);
+        }
+
+        public override string ToString()
+        {
+            if (IsInfinite)
+            {
+                return positive ? "+∞" : "-∞";
+            }
+            return string.Empty + Finite.ToString();
+        }
+
+        public static implicit operator Infinity<T>(T? value)
+        {
+            return new Infinity<T>(value, true);
+        }
+
+        public static implicit operator T?(Infinity<T> value)
+        {
+            return value.Finite;
+        }
+
+        public static Infinity<T> operator -(Infinity<T> value)
+        {
+            return new Infinity<T>(false);
+        }
+
+        public static Infinity<T> operator +(Infinity<T> value)
+        {
+            return new Infinity<T>(true);
+        }
+
+        public static bool operator ==(Infinity<T> left, Infinity<T> right)
+        {
+            return left.IsInfinite && right.IsInfinite && left.positive == right.positive
+                || !left.IsInfinite && !right.IsInfinite && left.Finite.Equals(right.Finite);
+        }
+
+        public static bool operator !=(Infinity<T> left, Infinity<T> right)
+        {
+            return !left.Finite.Equals(right.Finite) && left.positive != right.positive;
+        }
+
+        public static bool operator >(Infinity<T> left, Infinity<T> right)
+        {
+            return left.CompareTo(right) == 1;
+        }
+
+        public static bool operator <(Infinity<T> left, Infinity<T> right)
+        {
+            return left.CompareTo(right) == -1;
+        }
+
+        public static bool operator >=(Infinity<T> left, Infinity<T> right)
+        {
+            return left == right || left > right;
+        }
+
+        public static bool operator <=(Infinity<T> left, Infinity<T> right)
+        {
+            return left == right || left < right;
+        }
+    }
+
+    public static class Infinity
+    {
+        public static Infinity<T> Inf<T>(T? value = null, bool positive = true)
+            where T : struct, IComparable<T>, IComparable
+        {
+            return new Infinity<T>(value, positive);
+        }
+
+        public static Infinity<T> Inf<T>(bool positive)
+            where T : struct, IComparable<T>, IComparable
+        {
+            return new Infinity<T>(positive);
+        }
+
+        public static Infinity<T> Inf<T>()
+            where T : struct, IComparable<T>, IComparable
+        {
+            return new Infinity<T>();
+        }
+
+        public static Infinity<T> ToInfinity<T>(this T? value, bool positive)
+            where T : struct, IComparable<T>, IComparable
+        {
+            return new Infinity<T>(value, positive);
+        }
+
+        public static Infinity<T> ToPositiveInfinity<T>(this T? value)
+            where T : struct, IComparable<T>, IComparable
+        {
+            return new Infinity<T>(value, true);
+        }
+
+        public static Infinity<T> ToNegativeInfinity<T>(this T? value)
+            where T : struct, IComparable<T>, IComparable
+        {
+            return new Infinity<T>(value, false);
+        }
+    }
+}
---
+++ test/InfinityComparisonTests.cs
@@ -0,0 +1,132 @@
+namespace InifinityComparable.Tests
+{
+    public class InfinityComparisonTests
+    {
+        private static readonly Infinity<int> positiveInfinity = new Infinity<int>(null, true);
+        private static readonly Infinity<int> negativeInfinity = new Infinity<int>(null, false);
+        private static readonly Infinity<int> maxFinite = new Infinity<int>(int.MaxValue, true);
+        private static readonly Infinity<int> minFinite = new Infinity<int>(int.MinValue, true);
+        private static readonly Infinity<int> negativeMinFinite = new Infinity<int>(int.MinValue, false);
+        private static readonly Infinity<int> negativeMaxFinite = new Infinity<int>(int.MaxValue, false);
+
+
+        // Arrange
+        public static TheoryData<Infinity<int>, Infinity<int>, int> AllCompareToWithExpectedResult =
+            new TheoryData<Infinity<int>, Infinity<int>, int>
+            {
+                //{ positiveInfinity, positiveInfinity, 0 },
+                //{ negativeInfinity, negativeInfinity, 0 },
+                //{ positiveInfinity, negativeInfinity, 1 },
+                //{ negativeInfinity, positiveInfinity, -1 },
+                { minFinite, negativeMinFinite, 0 },
+                { maxFinite, negativeMaxFinite, 0 },
+                { positiveInfinity, maxFinite, 1 },
+                { negativeInfinity, minFinite, -1 },
+                { minFinite, negativeInfinity, 1 },
+                { maxFinite, positiveInfinity, -1 },
+            };
+
+        public static TheoryData<Infinity<int>, object?, int> AllCompareToObjectWithExpectedResult =
+            new TheoryData<Infinity<int>, object?, int>
+            {
+                { positiveInfinity, float.PositiveInfinity, 0 },
+                { positiveInfinity, double.PositiveInfinity, 0 },
+                { negativeInfinity, float.NegativeInfinity, 0 },
+                { negativeInfinity, double.NegativeInfinity, 0 },
+                { positiveInfinity, float.NegativeInfinity, 1 },
+                { positiveInfinity, double.NegativeInfinity, 1 },
+                { negativeInfinity, float.PositiveInfinity, -1 },
+                { negativeInfinity, double.PositiveInfinity, -1 }
+            };
+
+        [Theory]
+        [MemberData(nameof(AllCompareToWithExpectedResult))]
+        public void AllCompareTo_ShouldGiveExpectedResult(Infinity<int> a, Infinity<int> b, int expectedResult)
+        {
+            // Act
+            var result = a.CompareTo(b);
+
+            // Assert
+            result.Should().Be(expectedResult);
+        }
+
+        [Theory]
+        [MemberData(nameof(AllCompareToWithExpectedResult))]
+        [MemberData(nameof(AllCompareToObjectWithExpectedResult))]
+        public void AllCompareToObject_ShouldGiveExpectedResult(Infinity<int> a, object? b, int expectedResult)
+        {
+            // Act
+            var result = a.CompareTo(b);
+
+            // Assert
+            result.Should().Be(expectedResult);
+        }
+
+        [Theory]
+        [MemberData(nameof(AllCompareToWithExpectedResult))]
+        public void AllEquals_ShouldGiveExpectedResult(Infinity<int> a, Infinity<int> b, int compareResult)
+        {
+            // Act
+            var result = a.Equals(b);
+
+            // Assert
+            result.Should().Be(compareResult == 0);
+        }
+
+        [Theory]
+        [MemberData(nameof(AllCompareToWithExpectedResult))]
+        [MemberData(nameof(AllCompareToObjectWithExpectedResult))]
+        public void AllEqualsObject_ShouldGiveExpectedResult(Infinity<int> a, object? b, int compareResult)
+        {
+            // Act
+            var result = a.Equals(b);
+
+            // Assert
+            result.Should().Be(compareResult == 0);
+        }
+
+        [Theory]
+        [MemberData(nameof(AllCompareToWithExpectedResult))]
+        public void GreaterThan_ShouldGiveExpectedResult(Infinity<int> a, Infinity<int> b, int compareResult)
+        {
+            // Act
+            var result = a > b;
+
+            // Assert
+            result.Should().Be(compareResult == 1);
+        }
+
+        [Theory]
+        [MemberData(nameof(AllCompareToWithExpectedResult))]
+        public void SmallerThan_ShouldGiveExpectedResult(Infinity<int> a, Infinity<int> b, int compareResult)
+        {
+            // Act
+            var result = a < b;
+
+            // Assert
+            result.Should().Be(compareResult == -1);
+        }
+
+        [Theory]
+        [MemberData(nameof(AllCompareToWithExpectedResult))]
+        public void GreaterThanOrEquals_ShouldGiveExpectedResult(Infinity<int> a, Infinity<int> b, int compareResult)
+        {
+            // Act
+            var result = a >= b;
+
+            // Assert
+            result.Should().Be(compareResult >= 0);
+        }
+
+        [Theory]
+        [MemberData(nameof(AllCompareToWithExpectedResult))]
+        public void SmallerThanOrEquals_ShouldGiveExpectedResult(Infinity<int> a, Infinity<int> b, int compareResult)
+        {
+            // Act
+            var result = a <= b;
+
+            // Assert
+            result.Should().Be(compareResult <= 0);
+        }
+    }
+}
