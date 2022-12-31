a56af314d8c9793ee1354789217e51325d30bf4c
viclang <viclang.github@pm.me>
2022-12-31 02:31:40 +0100

    TimeOnly AddMinutes AddHours

--- src/InfinityComparable/Calculator/Add.cs
+++ src/InfinityComparable/Calculator/Add.cs
@@ -112,6 +112,24 @@ namespace InfinityComparable
 
         public static Infinity<TimeOnly> Add(this TimeOnly left, Infinity<TimeSpan> right)
             => Add(left, right, (x, y) => x.Add(y));
+
+        public static Infinity<TimeOnly> AddMinutes(this Infinity<TimeOnly> left, Infinity<double> right)
+            => Add(left, right, (x, y) => x.AddMinutes(y));
+
+        public static Infinity<TimeOnly> AddMinutes(this Infinity<TimeOnly> left, double right)
+            => Add(left, right, (x, y) => x.AddMinutes(y));
+
+        public static Infinity<TimeOnly> AddMinutes(this TimeOnly left, Infinity<double> right)
+            => Add(left, right, (x, y) => x.AddMinutes(y));
+
+        public static Infinity<TimeOnly> AddHours(this Infinity<TimeOnly> left, Infinity<double> right)
+            => Add(left, right, (x, y) => x.AddHours(y));
+
+        public static Infinity<TimeOnly> AddHours(this Infinity<TimeOnly> left, double right)
+            => Add(left, right, (x, y) => x.AddHours(y));
+
+        public static Infinity<TimeOnly> AddHours(this TimeOnly left, Infinity<double> right)
+            => Add(left, right, (x, y) => x.AddHours(y));
         #endregion
 
         #region Timespan extensions
